98c5a66b5b93ad8991a1a0e1f67858f1d8ddc995
shaunsaker <shaun@aux.co.za>
2019-04-26 13:36:08 +0200

    Update Modal stories

--- src/app/components/Modal/__tests__/index.stories.js
+++ src/app/components/Modal/__tests__/index.stories.js
@@ -5,18 +5,30 @@ import Modal from '..';
 
 const title = 'Forgot your password?';
 const description = "Enter your email address and we'll send you a password reset email.";
+const fields = [
+  {
+    type: 'email',
+    name: 'email',
+    label: 'Email Address',
+    required: true,
+  },
+];
 const form = {
-  fields: [
-    {
-      type: 'email',
-      name: 'email',
-      label: 'Email Address',
-      required: true,
-    },
-  ],
+  fields,
   handleSubmit: null,
 };
 
-storiesOf('Components|Modal', module).add('default', () => (
-  <Modal title={title} description={description} form={form} isOpen handleClose={null} />
-));
+storiesOf('Components|Modal', module)
+  .add('default', () => (
+    <Modal title={title} description={description} form={form} isOpen handleClose={null} />
+  ))
+  .add('without Description', () => <Modal title={title} form={form} isOpen handleClose={null} />)
+  .add('without Form fields', () => (
+    <Modal
+      title={title}
+      description={description}
+      form={{ fields: [], handleSubmit: null }}
+      isOpen
+      handleClose={null}
+    />
+  ));
