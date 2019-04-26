004a9087cbb84bdd40fe351d46e75795f65ac4ab
shaunsaker <shaun@aux.co.za>
2019-04-26 14:42:47 +0200

    Add blank ListItem component

---
+++ src/app/components/ListItem/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './styles';
+
+const ListItem = () => {
+  return (
+    <div>
+      <style jsx>{styles}</style>
+    </div>
+  );
+};
+
+ListItem.propTypes = {};
+ListItem.defaultProps = {};
+
+export default ListItem;
