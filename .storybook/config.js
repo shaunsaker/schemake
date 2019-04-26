ac0b44e870d5fd915eef332cda51b2a14ddd78d0
shaunsaker <shaun@aux.co.za>
2019-04-26 07:57:23 +0200

    Add mui theme to Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { muiTheme } from 'storybook-addon-material-ui';
+
+import { theme } from '../src/app/static/styles';
 
 // automatically import all files ending in *.stories.js
 const components = require.context('../src/app', true, /.stories.js$/);
@@ -6,5 +9,7 @@ const components = require.context('../src/app', true, /.stories.js$/);
 function loadStories() {
   components.keys().forEach((filename) => components(filename));
 }
+
+addDecorator(muiTheme(theme));
 
 configure(loadStories, module);
