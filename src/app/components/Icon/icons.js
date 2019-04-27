466ec1e302d688508c8db0aa7f07b9e068d8434d
shaunsaker <shaun@aux.co.za>
2019-04-27 10:04:30 +0200

    Update Icon component

--- src/app/components/Icon/icons.js
+++ src/app/components/Icon/icons.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Add, MoreVert } from '@material-ui/icons';
+import { AccountCircle, Add, MoreVert, Share, Sort } from '@material-ui/icons';
 
 const icons = {
+  'account-circle': (props) => <AccountCircle {...props} />,
   add: (props) => <Add {...props} />,
   menu: (props) => <MoreVert {...props} />,
+  share: (props) => <Share {...props} />,
+  sort: (props) => <Sort {...props} />,
 };
 
 export default icons;
