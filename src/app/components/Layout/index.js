c9a6c2962703df65cfed765adb39b7e459593354
shaunsaker <shaun@aux.co.za>
2019-04-28 08:46:05 +0200

    Remove Head component from Layout component

--- src/app/components/Layout/index.js
+++ src/app/components/Layout/index.js
@@ -3,15 +3,12 @@ import PropTypes from 'prop-types';
 
 import styles from './styles';
 
-import Head from '../Head';
 import HeaderBar from '../HeaderBar';
 import FooterBar from '../FooterBar';
 
-const Layout = ({ title, description, children }) => {
+const Layout = ({ children }) => {
   return (
     <div className="container">
-      <Head title={title} description={description} />
-
       <HeaderBar />
 
       <div className="content-container">{children}</div>
@@ -26,8 +23,6 @@ const Layout = ({ title, description, children }) => {
 };
 
 Layout.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string,
   children: PropTypes.node,
 };
 Layout.defaultProps = {};
