53e2284013d7e205361a42bcc0d767565a4fa0ff
shaunsaker <shaun@aux.co.za>
2019-05-01 16:31:15 +0200

    Fix createUserWithEmailAndPassword service

--- src/app/services/auth/createUserWithEmailAndPassword/index.js
+++ src/app/services/auth/createUserWithEmailAndPassword/index.js
@@ -4,13 +4,17 @@ export default ({ email, password }) => {
   return new Promise(async (resolve, reject) => {
     const fb = await firebase();
 
-    fb.auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then((response) => {
-        resolve(response);
-      })
-      .catch((error) => {
-        reject(error);
-      });
+    try {
+      fb.auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    } catch (error) {
+      reject(error);
+    }
   });
 };
