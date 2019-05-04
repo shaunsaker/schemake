d16032f0de617791bc773e40ac95235365bf5bf7
shaunsaker <shaun@aux.co.za>
2019-05-04 10:43:02 +0200

    Update DangerZone UI

--- src/app/layouts/tabs/profile/DangerZone/DangerZone.js
+++ src/app/layouts/tabs/profile/DangerZone/DangerZone.js
@@ -3,11 +3,20 @@ import PropTypes from 'prop-types';
 
 import styles from './styles';
 
+import Typography from '../../../../components/Typography';
 import PrimaryButton from '../../../../components/PrimaryButton';
 
 const DangerZone = ({ handleDeleteButtonClick }) => {
   return (
     <div className="container">
+      <div className="text-container">
+        <Typography type="heading" gutterBottom>
+          Delete your user account?
+        </Typography>
+
+        <Typography type="paragraph">Once you delete your user, there is no going back.</Typography>
+      </div>
+
       <div className="button-container">
         <PrimaryButton handleClick={handleDeleteButtonClick}>DELETE YOURSELF</PrimaryButton>
       </div>
