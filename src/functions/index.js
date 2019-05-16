4380d06c10d11a773366f0b6fa28ff7bb7c8c86d
shaunsaker <shaun@aux.co.za>
2019-05-16 17:43:22 +0200

    Fix functions imports

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,5 +1,7 @@
-const onDeleteUser = require('./onDeleteUser');
+const onDeleteUserAdded = require('./onDeleteUserAdded');
+const onInviteAdded = require('./onDeleteUserAdded');
 const onSupportAdded = require('./onSupportAdded');
 
 exports.onDeleteUser = onDeleteUser;
+exports.onInviteAdded = onInviteAdded;
 exports.onSupportAdded = onSupportAdded;
