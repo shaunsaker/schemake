eb10430327793753d5f928fc7f548ec7aa4f480f
shaunsaker <shaun@aux.co.za>
2019-05-16 17:40:01 +0200

    Add onInviteAdded function

--- src/functions/config/index.js
+++ src/functions/config/index.js
@@ -1,7 +1,17 @@
 const config = {
   appName: 'schemake',
   appEmail: 'sakershaun@gmail.com', // TODO: Update this
+  unsubscribe: {
+    address: 'Blouberg',
+    city: 'Cape Town',
+    state: 'Western Cape',
+    zip: 7439,
+  },
+  appLink: 'https://www.schemake.com',
   sendGridAPIKey: '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',
+  templates: {
+    invite: 'd-4e0b9092297f4b52958c69f41da30703',
+  },
 };
 
 module.exports = config;
---
+++ src/functions/onInviteAdded/index.js
@@ -0,0 +1,33 @@
+const functions = require('firebase-functions');
+const sgMail = require('@sendgrid/mail');
+
+const config = require('../config');
+
+sgMail.setApiKey(config.sendGridAPIKey); // FIXME: Use .env
+
+const onInviteAdded = functions.firestore.document('_invites/{inviteId}').onCreate((snapshot) => {
+  const { email, teamId, invitee } = snapshot.data();
+  const mailOptions = {
+    from: `${config.appName} <${config.appEmail}>`,
+    to: email,
+    templateId: config.templates.invite,
+    dynamic_template_data: {
+      Sender_Name: config.appName,
+      Sender_Address: config.unsubscribe.address,
+      Sender_City: config.unsubscribe.city,
+      Sender_State: config.unsubscribe.state,
+      Sender_Zip: config.unsubscribe.zip,
+      subject: 'You were added to a team on schemake',
+      invitee,
+      schemakeLink: config.appLink,
+      inviteLink: `${config.appLink}/signUp?teamId=${teamId}`,
+    },
+  };
+
+  return sgMail
+    .send(mailOptions)
+    .then(() => console.log('Email sent successfully'))
+    .catch((error) => console.log(error.message));
+});
+
+module.exports = onInviteAdded;
