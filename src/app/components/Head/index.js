c41e765917e9275cc3a6dd5a63435144ca3a19f7
shaunsaker <shaun@aux.co.za>
2019-05-17 07:07:04 +0200

    Only add the Drift chat widget in production

--- src/app/components/Head/index.js
+++ src/app/components/Head/index.js
@@ -19,6 +19,42 @@ const HeadComponent = ({ title, description, router }) => {
   const titleToUse = title || (route && route.title) || defaultTitle;
   const descriptionToUse = description || (route && route.description) || defaultDescription;
 
+  /*
+   * Add the Drift chat widget if not in dev
+   */
+  const isProduction = process.env.REACT_APP_ENV === 'prod';
+  const driftChatWidget = isProduction && (
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          "use strict";
+          !function() {
+          var t = window.driftt = window.drift = window.driftt || [];
+          if (!t.init) {
+          if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+          t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
+          t.factory = function(e) {
+          return function() {
+            var n = Array.prototype.slice.call(arguments);
+            return n.unshift(e), t.push(n), t;
+          };
+          }, t.methods.forEach(function(e) {
+          t[e] = t.factory(e);
+          }), t.load = function(t) {
+          var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+          o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+          var i = document.getElementsByTagName("script")[0];
+          i.parentNode.insertBefore(o, i);
+          };
+          }
+          }();
+          drift.SNIPPET_VERSION = '0.3.1';
+          drift.load('bpru8mmuag5d');
+        `,
+      }}
+    />
+  );
+
   return (
     <Head>
       {/* Title and description */}
@@ -99,37 +135,9 @@ const HeadComponent = ({ title, description, router }) => {
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700"
       />
 
+      {driftChatWidget}
+
       <noscript>Your browser does not support JavaScript!</noscript>
-
-      {/* Drift Chat widget */}
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `"use strict";
-
-!function() {
-  var t = window.driftt = window.drift = window.driftt || [];
-  if (!t.init) {
-    if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
-    t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
-    t.factory = function(e) {
-      return function() {
-        var n = Array.prototype.slice.call(arguments);
-        return n.unshift(e), t.push(n), t;
-      };
-    }, t.methods.forEach(function(e) {
-      t[e] = t.factory(e);
-    }), t.load = function(t) {
-      var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
-      o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
-      var i = document.getElementsByTagName("script")[0];
-      i.parentNode.insertBefore(o, i);
-    };
-  }
-}();
-drift.SNIPPET_VERSION = '0.3.1';
-drift.load('bpru8mmuag5d');`,
-        }}
-      />
     </Head>
   );
 };
