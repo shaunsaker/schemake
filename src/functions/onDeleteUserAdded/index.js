087f07094f8ebb3cb401b2ac8e854fc4dcd3d1a5
shaunsaker <shaun@aux.co.za>
2019-05-20 07:44:21 +0200

    Functions use shared admin app

--- src/functions/onDeleteUserAdded/index.js
+++ src/functions/onDeleteUserAdded/index.js
@@ -1,7 +1,6 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 
-admin.initializeApp({});
+const admin = require('../firebase');
 
 const onDeleteUserAdded = functions.firestore
   .document('_deleteUsers/{uid}')
--- src/functions/onUserAdded/index.js
+++ src/functions/onUserAdded/index.js
@@ -1,7 +1,6 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 
-admin.initializeApp({});
+const admin = require('../firebase');
 
 const db = admin.firestore();
 
