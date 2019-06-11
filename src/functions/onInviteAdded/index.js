61b556adb8c0388d6782de6ae7eccc38de4946d3
Shaun Saker <shaun@aux.co.za>
2019-06-11 16:22:56 +0200

    Add existing user to team in onInviteAdded

--- src/functions/onInviteAdded/index.js
+++ src/functions/onInviteAdded/index.js
@@ -1,24 +1,67 @@
 const functions = require('firebase-functions');
 
+const admin = require('../firebase');
 const config = require('../config');
 const sendEmail = require('../sendEmail');
 
-const onInviteAdded = functions.firestore.document('_invites/{inviteId}').onCreate((snapshot) => {
-  const values = snapshot.data();
-  const { email, invitee } = values;
-  const emailOptions = {
-    from: `${config.appUsername} <${config.appEmail}>`,
-    to: email,
-    template: 'invite',
-    locals: {
-      invitee,
-      appName: config.appName,
-      appLink: config.appLink,
-      signupLink: `${config.appLink}/signup`,
-    },
-  };
+const db = admin.firestore();
 
-  return sendEmail(emailOptions);
-});
+const onInviteAdded = functions.firestore
+  .document('_invites/{inviteId}')
+  .onCreate(async (snapshot) => {
+    /*
+     * Send the person their email invite
+     */
+    const values = snapshot.data();
+    const { email, invitee } = values;
+    const emailOptions = {
+      from: `${config.appUsername} <${config.appEmail}>`,
+      to: email,
+      template: 'invite',
+      locals: {
+        invitee,
+        appName: config.appName,
+        appLink: config.appLink,
+        signupLink: `${config.appLink}/signup`,
+      },
+    };
+
+    await sendEmail(emailOptions);
+
+    console.log('Email sent successfully');
+
+    /*
+     * Check if the person is signed up already
+     * If so, add them to the team directly
+     */
+    const users = await db
+      .collection('users')
+      .where('email', '==', email)
+      .get();
+    const user = users.docs[0];
+
+    if (user) {
+      const { id: uid } = user;
+
+      /*
+       * Add the user's uid to the users field of the team doc
+       */
+      const { teamId } = values;
+      const teamRef = db.collection('teams').doc(teamId);
+
+      await teamRef.update({ users: admin.firestore.FieldValue.arrayUnion(uid) });
+
+      console.log('User added to existing team successfully.');
+
+      /*
+       * Add the teamId to the teams field of the user doc
+       */
+      const userRef = db.collection('users').doc(uid);
+
+      await userRef.update({ teams: admin.firestore.FieldValue.arrayUnion(teamId) });
+
+      console.log('Team added to existing user successfully.');
+    }
+  });
 
 module.exports = onInviteAdded;
