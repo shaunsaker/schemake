5acd751971e9883da38f7b9c3d2e2716d410daef
Shaun Saker <shaun@aux.co.za>
2019-06-12 19:18:55 +0200

    Really delete all of users data in the onDeleteUserAdded function

--- src/functions/onDeleteUserAdded/index.js
+++ src/functions/onDeleteUserAdded/index.js
@@ -2,24 +2,66 @@ const functions = require('firebase-functions');
 
 const admin = require('../firebase');
 
+const db = admin.firestore();
+
 const onDeleteUserAdded = functions.firestore
   .document('_deleteUsers/{uid}')
-  .onCreate((snapshot) => {
+  .onCreate(async (snapshot) => {
     const { id: uid } = snapshot;
+    /*
+     * Delete the auth user
+     */
+    await admin.auth().deleteUser(uid);
 
-    admin
-      .auth()
-      .deleteUser(uid)
-      .then(() => {
-        console.log('Successfully deleted user', { uid });
+    console.log('Successfully deleted user account', { uid });
 
-        return null;
-      })
-      .catch((error) => {
-        console.log('Error deleting user:', error, { uid });
+    /*
+     * Delete the user's data
+     */
+    await db
+      .collection('users')
+      .doc(uid)
+      .delete();
 
-        return error;
-      });
+    console.log('Successfully deleted user data', { uid });
+
+    /*
+     * Delete the user's teams data
+     */
+    const teamsDocs = await db
+      .collection('teams')
+      .where('createdBy', '==', uid)
+      .get();
+
+    teamsDocs.forEach(async (teamDoc) => {
+      const { id } = teamDoc;
+
+      await db
+        .collection('teams')
+        .doc(id)
+        .delete();
+    });
+
+    console.log('Successfully deleted teams data', { uid });
+
+    /*
+     * Delete the user's projects data
+     */
+    const projectsDocs = await db
+      .collection('projects')
+      .where('createdBy', '==', uid)
+      .get();
+
+    projectsDocs.forEach(async (projectDoc) => {
+      const { id } = projectDoc;
+
+      await db
+        .collection('projects')
+        .doc(id)
+        .delete();
+    });
+
+    console.log('Successfully deleted projects data', { uid });
   });
 
 module.exports = onDeleteUserAdded;
