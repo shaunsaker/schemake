4ee0feedf8a6171479fda7ff0c601b72da5c0e56
Shaun Saker <shaun@aux.co.za>
2019-06-19 08:11:48 +0200

    Storybook DangerZone page

---
+++ src/app/layouts/pages/Profile/DangerZone/__stories__/DangerZone.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import DangerZone from '../DangerZone';
+
+storiesOf('Layouts|DangerZone', module).add('default', () => <DangerZone />);
