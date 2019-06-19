d0a2621b749d316305e60080c7893164ce140c64
Shaun Saker <shaun@aux.co.za>
2019-06-19 08:18:54 +0200

    Use Error page in ErrorHandler

--- src/app/layouts/pages/Error/__stories__/Error.stories.js
+++ src/app/layouts/pages/Error/__stories__/Error.stories.js
@@ -3,4 +3,6 @@ import { storiesOf } from '@storybook/react';
 
 import Error from '..';
 
-storiesOf('Layouts|Error', module).add('default', () => <Error />);
+storiesOf('Layouts|Error', module)
+  .add('default', () => <Error />)
+  .add('custom', () => <Error errorMessage="Insufficient permissions." />);
