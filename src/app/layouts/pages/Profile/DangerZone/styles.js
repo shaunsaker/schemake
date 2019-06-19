ab0a40b04c50e352f87c3d436958a4bac7f3979e
Shaun Saker <shaun@aux.co.za>
2019-06-19 11:09:46 +0200

    Give tabs min-height

--- src/app/layouts/pages/Profile/DangerZone/styles.js
+++ src/app/layouts/pages/Profile/DangerZone/styles.js
@@ -4,6 +4,7 @@ import { rhythm } from '../../../../static/styles/styleConstants';
 
 const styles = css`
   .container {
+    min-height: 100vh; /* so that we don't see a jump on load of tabs data */
   }
 
   .text-container {
