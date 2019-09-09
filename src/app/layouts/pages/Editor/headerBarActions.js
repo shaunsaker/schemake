faee16ae7bb486f751718085abff2c1f9bf35d57
Shaun Saker <shaun@aux.co.za>
2019-09-09 12:05:24 +0200

    Editor/Update header bar actions

--- src/app/layouts/pages/Editor/headerBarActions.js
+++ src/app/layouts/pages/Editor/headerBarActions.js
@@ -1,13 +1,5 @@
 const headerBarActions = [
   {
-    iconName: 'add',
-    tooltip: 'Add new collection',
-  },
-  {
-    iconName: 'sort',
-    tooltip: 'Sort mode (collapse collections)',
-  },
-  {
     iconName: 'share',
     tooltip: 'Share this project',
   },
