33074855e6ba8467496cab13ba5ece2066333533
Shaun Saker <shaun@aux.co.za>
2019-09-26 09:46:10 +0200

    Add hotjar

--- src/app/components/Head/index.js
+++ src/app/components/Head/index.js
@@ -11,6 +11,55 @@ import { colors } from '../../static/styles/styleConstants';
 const primaryColor = colors.primary;
 const defaultTitle = SEO.title;
 const defaultDescription = SEO.description;
+const driftScript = `
+  "use strict";
+  !function() {
+  var t = window.driftt = window.drift = window.driftt || [];
+  if (!t.init) {
+  if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+  t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
+  t.factory = function(e) {
+  return function() {
+    var n = Array.prototype.slice.call(arguments);
+    return n.unshift(e), t.push(n), t;
+  };
+  }, t.methods.forEach(function(e) {
+  t[e] = t.factory(e);
+  }), t.load = function(t) {
+  var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+  o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+  var i = document.getElementsByTagName("script")[0];
+  i.parentNode.insertBefore(o, i);
+  };
+  }
+  }();
+  drift.SNIPPET_VERSION = '0.3.1';
+  drift.load('bpru8mmuag5d');
+  `;
+const hotjarScript = `
+  (function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:1501918,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`;
+const isProduction = process.env.REACT_APP_ENV === 'prod';
+let driftWidget;
+let hotjar;
+
+if (isProduction) {
+  driftWidget = (
+    <script
+      dangerouslySetInnerHTML={{
+        __html: driftScript,
+      }}
+    />
+  );
+
+  hotjar = <script dangerouslySetInnerHTML={{ __html: hotjarScript }} />;
+}
 
 const HeadComponent = ({ title, description, router }) => {
   const { pathname } = router;
@@ -18,42 +67,6 @@ const HeadComponent = ({ title, description, router }) => {
   const route = routesArray.filter((item) => item.href === pathname)[0];
   const titleToUse = title || (route && route.title) || defaultTitle;
   const descriptionToUse = description || (route && route.description) || defaultDescription;
-
-  /*
-   * Add the Drift chat widget if not in dev
-   */
-  const isProduction = process.env.REACT_APP_ENV === 'prod';
-  const driftChatWidget = isProduction && (
-    <script
-      dangerouslySetInnerHTML={{
-        __html: `
-          "use strict";
-          !function() {
-          var t = window.driftt = window.drift = window.driftt || [];
-          if (!t.init) {
-          if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
-          t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
-          t.factory = function(e) {
-          return function() {
-            var n = Array.prototype.slice.call(arguments);
-            return n.unshift(e), t.push(n), t;
-          };
-          }, t.methods.forEach(function(e) {
-          t[e] = t.factory(e);
-          }), t.load = function(t) {
-          var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
-          o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
-          var i = document.getElementsByTagName("script")[0];
-          i.parentNode.insertBefore(o, i);
-          };
-          }
-          }();
-          drift.SNIPPET_VERSION = '0.3.1';
-          drift.load('bpru8mmuag5d');
-        `,
-      }}
-    />
-  );
 
   return (
     <Head>
@@ -135,7 +148,9 @@ const HeadComponent = ({ title, description, router }) => {
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700"
       />
 
-      {driftChatWidget}
+      {driftWidget}
+
+      {hotjar}
 
       <noscript>Your browser does not support JavaScript!</noscript>
     </Head>
